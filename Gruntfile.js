11b9f43d18ab99f9869d55d31660112e9412e6eb
nikuz <micurino@gmail.com>
2015-12-30 01:24:48 +0530

    Removed webdriver dependencies

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,8 +1,6 @@
 'use strict';
 
-var seleniumServer = require('selenium-standalone'),
-  path = require('path'),
-  webpack = require('webpack'),
+var webpack = require('webpack'),
   WebpackDevServer = require('webpack-dev-server'),
   webpackConfig = require('./webpack.config.js'),
   htmlPlugin = require('html-webpack-plugin'),
@@ -121,67 +119,6 @@ module.exports = function(grunt) {
     }).listen(8080, 'localhost', function(err) {
       if (err) throw new Error(err);
     });
-  });
-
-  var seleniumProcess;
-  grunt.task.registerTask('seleniumInstall', function() {
-    var done = this.async();
-    seleniumServer.install({
-      baseURL: 'http://selenium-release.storage.googleapis.com',
-      version: '2.48.2'
-    }, done);
-  });
-  grunt.task.registerTask('seleniumStart', function() {
-    var done = this.async();
-    seleniumServer.start({}, function(err, response) {
-      if (err) {
-        done(err);
-      } else {
-        seleniumProcess = response;
-        done();
-      }
-    });
-  });
-  grunt.task.registerTask('seleniumStop', function() {
-    seleniumProcess.kill();
-  });
-  grunt.task.registerTask('restoreConfig', function() {
-    var oldConfFile = `${wdioConfFile}.old`;
-    if (grunt.file.exists(oldConfFile)) {
-      grunt.file.copy(oldConfFile, wdioConfFile);
-      grunt.file.delete(oldConfFile);
-    }
-  });
-
-  grunt.task.registerTask('crawler', function() {
-    grunt.option('force', true);
-    process.on('SIGINT', () => {
-      this.async()(false);
-    });
-    var tasks = [
-        'seleniumInstall',
-        'seleniumStart',
-        'webdriver',
-        'seleniumStop'
-      ],
-      target = grunt.option('target'),
-      wdioConf = require(wdioConfFile).config;
-
-    if (!target) {
-      return grunt.fail.warn('`target` is required');
-    } else if (!grunt.file.exists(`./app/sites/${target}.js`)) {
-      return grunt.fail.warn(`target doesn't exist`);
-    }
-
-    wdioConf.specs = [`./app/sites/${target}.js`];
-
-    let confBackupFile = `${wdioConfFile}.old`;
-    grunt.file.copy(wdioConfFile, confBackupFile);
-    grunt.file.write(wdioConfFile, 'exports.config = ' + JSON.stringify(wdioConf, null, 2));
-    tasks.push('restoreConfig');
-
-    console.log('Started crawler for `%s`', target);
-    grunt.task.run(tasks);
   });
 
   grunt.registerTask('default', [
